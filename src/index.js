150d083c0be7109c7d0f67c291116d771676d36b
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-05-02 10:56:42 +0200

    feat: add transition and final styles

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.contains("hidden")
+    ? menu.classList.remove("hidden")
+    : menu.classList.add("hidden");
+  // menu.classList.toggle("hidden");
+});
